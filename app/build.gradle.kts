6e536e699df5d80781a84cc44be34b655c878256
jose nasser <jose.nasser@softsvgroup.com>
2025-01-23 23:40:44 -0600

    Upload libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kapt)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.safeArgs)
 }
 
 android {
@@ -15,6 +18,10 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures {
+        dataBinding = true
     }
 
     buildTypes {
@@ -42,6 +49,34 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.fragment.ktx)
+
+    implementation(libs.retrofit)
+    implementation(libs.gson.converter)
+    implementation(libs.okHttp)
+    implementation(libs.okHttp.interceptor)
+
+    implementation(libs.hilt)
+    implementation(libs.hilt.compiler)
+    implementation(libs.hilt.worker)
+    implementation(libs.hilt.annotation)
+
+    implementation(libs.viewmodel)
+    implementation(libs.livedata)
+    implementation(libs.lifecycle)
+
+    implementation(libs.room)
+    implementation(libs.room.runtime)
+
+    implementation(libs.coroutines)
+
+    implementation(libs.navigation)
+    implementation(libs.navigation.ui)
+
+    implementation(libs.worrManager)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
