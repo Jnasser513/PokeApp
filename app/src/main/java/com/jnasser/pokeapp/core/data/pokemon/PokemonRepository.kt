fe760cd07fd492ed03c33bd922a30e748cf48cf9
jose nasser <jose.nasser@softsvgroup.com>
2025-01-25 01:47:07 -0600

    Create function in viewmodel to insert pokemon list

--- app/src/main/java/com/jnasser/pokeapp/core/data/pokemon/PokemonRepository.kt
+++ app/src/main/java/com/jnasser/pokeapp/core/data/pokemon/PokemonRepository.kt
@@ -1,5 +1,6 @@
 package com.jnasser.pokeapp.core.data.pokemon
 
+import com.jnasser.pokeapp.core.data.RoomResponse
 import com.jnasser.pokeapp.core.domain.LocalPokemonDataSource
 import com.jnasser.pokeapp.pokemonList.data.PokemonResponse
 import javax.inject.Inject
@@ -8,9 +9,9 @@ class PokemonRepository @Inject constructor(
     private val localPokemonDataSource: LocalPokemonDataSource
 ) {
 
-    suspend fun insertPokemons(pokemonList: List<PokemonResponse>) {
+    suspend fun insertPokemons(pokemonList: List<PokemonResponse>): RoomResponse<List<Long>> {
         val mappedPokemonList = pokemonList.toPokemonList()
-        localPokemonDataSource.insertPokemonList(mappedPokemonList)
+        return localPokemonDataSource.insertPokemonList(mappedPokemonList)
     }
 
     suspend fun getAllPokemons() = localPokemonDataSource.getPokemonList()
--- app/src/main/java/com/jnasser/pokeapp/pokemonList/presentation/PokemonListViewModel.kt
+++ app/src/main/java/com/jnasser/pokeapp/pokemonList/presentation/PokemonListViewModel.kt
@@ -1,14 +1,36 @@
 package com.jnasser.pokeapp.pokemonList.presentation
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jnasser.pokeapp.core.data.pokemon.Pokemon
+import com.jnasser.pokeapp.core.data.pokemon.PokemonRepository
+import com.jnasser.pokeapp.core.di.IoDispatcher
+import com.jnasser.pokeapp.core.presentation.PresentationUtils
+import com.jnasser.pokeapp.core.presentation.UIStatus
+import com.jnasser.pokeapp.core.usecases.InsertPokemonListUseCase
+import com.jnasser.pokeapp.pokemonList.data.PokemonResponse
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class PokemonListViewModel @Inject constructor(
-
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
+    private val insertPokemonListUseCase: InsertPokemonListUseCase
 ): ViewModel() {
 
-    private val _statusGetPokemonList = MutableLiveData<>
+    private val _statusInsertPokemonList = MutableLiveData<UIStatus<List<Long>>>()
+    val statusInsertPokemonList: LiveData<UIStatus<List<Long>>> get() = _statusInsertPokemonList
+
+    fun insertPokemonList(pokemonList: List<PokemonResponse>) {
+        _statusInsertPokemonList.value = UIStatus.Loading("Cargando...")
+        viewModelScope.launch(ioDispatcher) {
+            _statusInsertPokemonList.postValue(
+                PresentationUtils.mapRoomResponseToUIStatus(insertPokemonListUseCase.invoke(pokemonList))
+            )
+        }
+    }
 }
